81533012c7e063444dba36157f43109b67a697ac
Yuvanesh-ux <68208096+Yuvanesh-ux@users.noreply.github.com>
2023-03-22 08:03:20 -0400

    Multiprocessing

--- prompt-scrape/mapper.py
+++ prompt-scrape/mapper.py
@@ -2,19 +2,21 @@ from nomic import atlas
 import jsonlines
 
 documents = []
-for item in ["output_data/output_1000_2000.jsonl", "output_data/output_2000_3000.jsonl", "output_data/output_3000_4000.jsonl"]:
+for item in ["output_data/output_1000_2000.jsonl", "output_data/output_2000_3000.jsonl", "output_data/output_3000_4000.jsonl", "output_data/output_4000_14000.jsonl"]:
     with jsonlines.open(f"{item}", mode="r") as reader:
         for datum in reader:
-            del datum["model_settings"]
-            if any(isinstance(v, dict) for v in datum.values()) or datum["response"] is None:
+            try:
+                del datum["model_settings"]
+                if any(isinstance(v, dict) for v in datum.values()) or datum["response"] is None:
+                    continue
+                documents.append(datum)
+            except jsonlines.jsonlines.InvalidLineError:
                 continue
-
-            documents.append(datum)
 
 
 
 project = atlas.map_text(data=documents,
                           indexed_field='response',
-                          name='ChatGPT 3k Output LAION OIB V2',
-                          description='ChatGPT 3k Output (Max Token Update)'
+                          name='ChatGPT Output LAION OIB V3',
+                          description='ChatGPT Output (Multhreading Update)'
                         )
--- prompt-scrape/scrape.py
+++ prompt-scrape/scrape.py
@@ -9,8 +9,10 @@ from loguru import logger
 from timeit import default_timer as timer
 from dotenv import load_dotenv
 import os
+import concurrent
+import concurrent.futures
 import multiprocessing
-import time
+
 
 load_dotenv()
 
@@ -28,42 +30,46 @@ prompt_template = PromptTemplate(
     partial_variables={"format_instructions": format_instructions}
 )
 
-model = OpenAIChat(model_name="gpt-3.5-turbo", openai_api_key=os.environ['OPENAI_API_KEY'], model_kwargs={"max_tokens": -1})
+openai_api_keys = [os.environ[f'OPENAI_API_KEY{i}'] for i in range(1, 11)]
+
+num_workers = 10
+shard_size = 200
 
 class Scraper():
     def __init__(self) -> None:
         pass
-
-    def get_responses(self, prompt: str, model_settings: dict = {"max_tokens": -1}):
-        failed = ""
-        _input = prompt_template.format_prompt(query=prompt)
-
-        output = model(_input.to_string())
-        with jsonlines.open("output_data/output_4000_14000.jsonl", mode="a") as writer:
-            try:
-                json_data = output_parser.parse(output)
-                json_data["model_settings"] = model_settings
-                writer.write(json_data)
-            except (KeyboardInterrupt, ValueError, IndexError):
-                logger.warning("Something went wrong with this prompt! Skipping to next one")
-                with jsonlines.open("output_data/failed/fails.jsonl", mode="w") as writer:
-                    writer.write(failed)
             
+    def get_responses(self, all_prompts: List[dict], i: int, model_settings: dict = {"max_tokens": -1}):
+            prompts = all_prompts[i: i + shard_size]
+            model = OpenAIChat(model_name="gpt-3.5-turbo", openai_api_key=openai_api_keys[i%10], model_kwargs={"max_tokens": -1})
+            for prompt in tqdm(prompts):
+                _input = prompt_template.format_prompt(query=prompt)
+
+                output = model(_input.to_string())
+                with jsonlines.open("output_data/output_4000_x.jsonl", mode="a") as writer:
+                    try:
+                        json_data = output_parser.parse(output)
+                        json_data["model_settings"] = model_settings
+                        writer.write(json_data)
+                    except (KeyboardInterrupt, ValueError, IndexError):
+                        logger.warning("Something went wrong with this prompt! Skipping to next one")
+                        with jsonlines.open("output_data/failed/fails.jsonl", mode="w") as writer:
+                            writer.write(prompt)
 
     def scrape(self, all_prompts: List[dict]):
         logger.info("Generating Pairs")
-        
-        for prompt in tqdm(all_prompts):
-            p = multiprocessing.Process(target=self.get_responses, args=(prompt,))
-            p.start()
-            p.join(timeout=30)
 
-            if p.is_alive():
-                logger.warning("Iteration Took too long")
-                with jsonlines.open("output_data/failed/fails.jsonl", mode="a") as writer:
-                    writer.write(prompt)
-                p.terminate() # Terminate the process
-                p.join()
+        progress = tqdm(total=len(all_prompts) // shard_size)
+        with concurrent.futures.ProcessPoolExecutor(max_workers=num_workers) as executor:
+            futures = [executor.submit(self.get_responses, i=i, all_prompts=all_prompts) for i in range(0, len(all_prompts), shard_size)]
+
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    future.result()
+                except Exception as e:
+                    logger.exception(f"Error processing prompt: {e}")
+                    
+                progress.update(1)
 
 if __name__ == "__main__":
     scraper = Scraper()
@@ -81,6 +87,4 @@ if __name__ == "__main__":
 
             all_data.append(result)
 
-    openai_api_key = os.environ['OPENAI_API_KEY']
-
-    scraper.scrape(all_prompts=all_data[4000:14000])
+    scraper.scrape(all_prompts=all_data[35000:55000])
