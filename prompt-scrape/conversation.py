f06a56eaafc072498055c70d0eda09e7f0a6f07a
Yuvanesh-ux <68208096+Yuvanesh-ux@users.noreply.github.com>
2023-03-28 21:37:50 -0400

    clean

--- prompt-scrape/conversation.py
+++ prompt-scrape/conversation.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 
 load_dotenv()
 
-openai_api_keys = [os.environ[f'OPENAI_API_KEY{i}'] for i in range(6, 21)]
+openai_api_keys = [os.environ[f'OPENAI_API_KEY{i}'] for i in range(22, 26)]
 
 shard_size = 200
 num_workers = 10
@@ -21,23 +21,23 @@ class Conversation():
     def __init__(self) -> None:
         pass
 
-    def get_responses(self, all_prompts: List[str], i):
+    def get_responses(self, all_prompts: List[str], i: int, output_path: str = "", source: str = ""):
         prompts = all_prompts[i: i + shard_size]
         model = OpenAIChat(model_name="gpt-3.5-turbo", openai_api_key=openai_api_keys[random.randint(0,len(openai_api_keys)-1)], model_kwargs={"max_tokens": -1})
 
         for prompt in tqdm(prompts):
-            with jsonlines.open("output_data/stackoverflow_chunk_9.jsonl", mode="a") as writer:
+            with jsonlines.open(output_path, mode="a") as writer:
                 try:
                     turn_1 = model(prompt)
                     follow_up_prompt = 'Write an insightful follow up question given the previous context: '
                     question_2 = model(follow_up_prompt + prompt + turn_1)
                     turn_2_context = prompt + turn_1
                     turn_2 = model(turn_2_context + question_2)
-                    json_data = {"prompt" : prompt, "response_turn_1": turn_1, "prompt_turn_2": question_2, "response_turn_2": turn_2, "source": "pacovaldez/stackoverflow-questions"}
+                    json_data = {"prompt" : prompt, "response_turn_1": turn_1, "prompt_turn_2": question_2, "response_turn_2": turn_2, "source": source}
                     writer.write(json_data)
                 except:
                     logger.warning("something went wrong! next")
-                    with jsonlines.open("output_data/failed/chunk_9_conversation_fails.jsonl", mode="a") as writer:
+                    with jsonlines.open(f"{output_path}/fails.jsonl", mode="a") as writer:
                         writer.write(prompt)
 
     def scrape(self, all_prompts: List[str]):
@@ -55,11 +55,11 @@ class Conversation():
 
 if __name__ == "__main__":
     all_prompts = []
-    with jsonlines.open("input_prompts/stackoverflow/stackoverflow_chunk_9.jsonl", mode="r") as reader:
+    with jsonlines.open("input_prompts/laion_leftovers", mode="r") as reader:
         for idx, datum in enumerate(reader):
-            all_prompts.append(datum["body"])
+            all_prompts.append(datum["prompt"])
 
     conversation = Conversation()
-    conversation.scrape(all_prompts[0:100000])
+    conversation.scrape(all_prompts[0:10])
 
     
