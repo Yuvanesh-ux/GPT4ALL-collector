f105a46cf19c1b6be8235b6a207fa968943ebaf9
Yuvanesh-ux <68208096+Yuvanesh-ux@users.noreply.github.com>
2023-04-02 17:52:58 -0400

    mapper base

---
+++ prompt-scrape/mapper.py
@@ -0,0 +1,103 @@
+from nomic import atlas
+import jsonlines
+import os
+import re
+from tqdm import tqdm
+
+def abstract_infill():
+# abstract_infill
+    file_name = 'abstract_infill'
+    documents = []
+    with jsonlines.open(os.path.join("input_prompts", f"unified_{file_name}.jsonl"), mode='r') as reader:
+        for idx, item in enumerate(reader):
+            json = {}
+            try:
+                background = re.search(r'Background:([\s\S]*?)<human>', item['text']).group(1).strip()
+            except:
+                background = ""
+
+            # Extract each section between <human> and <bot>
+            sections = re.findall(r'<human>:(.*?)<bot>:(.*?)\n', item['text'], re.DOTALL)
+
+            json["background"] = ""
+            if len(background) > 0:
+                json["background"] = background
+            for i, section in enumerate(sections):
+                if i <= 5:
+                    json[f"turn_{i}_question"] = section[0].strip()
+                # print('Section', i+1, ':', section[0].strip())
+
+            documents.append(json)
+
+    # making every json have the same keys
+    for doc in documents:
+        for i in range (6):
+            if f"turn_{i}_question" not in doc:
+                doc[f"turn_{i}_question"] = ""
+
+    # map explorer
+    atlas.map_text(
+        data=documents,
+        indexed_field='turn_0_question',
+        name=f"{file_name} v3 prompts",
+        description=f'{file_name} multiturn map exploration',
+        organization_name="GPT4ALL"
+    )
+
+def multi_news():
+    file_name = 'multi_news'
+    documents = []
+    with jsonlines.open(os.path.join("input_prompts", f"unified_{file_name}.jsonl"), mode='r') as reader:
+        for idx, item in enumerate(reader):
+            if idx:
+                json = {}
+                # Extract each section between <human> and <bot>
+                result = re.search(r'(?<=<human>:).*?(?=<bot>)', item["text"], re.DOTALL)
+
+                json[f"turn_0_question"] = result.group(0).strip()
+                documents.append(json)
+
+    # making every json have the same keys
+    # for doc in documents:
+    #     for i in range(6):
+    #         if f"turn_{i}_question" not in doc:
+    #             doc[f"turn_{i}_question"] = ""
+
+    # with jsonlines.open("thing.jsonl", mode="a") as writer:
+    #     for doc in tqdm(documents):
+    #         writer.write(doc)
+
+    # map explorer
+    atlas.map_text(
+        data=documents,
+        indexed_field='turn_0_question',
+        name=f"{file_name} v4 prompts",
+        description=f'{file_name} map exploration',
+        # shard_size=100,
+        # organization_name="GPT4ALL"
+    )
+
+def multi_sum():
+    file_name = 'multi_sum'
+    documents = []
+    with jsonlines.open(os.path.join("input_prompts", f"unified_{file_name}.jsonl"), mode='r') as reader:
+        for idx, item in enumerate(reader):
+            if idx < 10_000:
+                json = {}
+                # Extract each section between <human> and <bot>
+                result = re.search(r'(?<=<human>:).*?(?=<bot>)', item["text"], re.DOTALL)
+
+                json[f"turn_0_question"] = result.group(0).strip()
+                documents.append(json)
+    
+    atlas.map_text(
+        data=documents,
+        indexed_field='turn_0_question',
+        name=f"{file_name} v4 prompts",
+        description=f'{file_name} map exploration',
+        # shard_size=100,
+        # organization_name="GPT4ALL"
+    )
+
+
+multi_sum()
