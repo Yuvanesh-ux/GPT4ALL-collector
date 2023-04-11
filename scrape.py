7f96e45b11d328107e1007ea1380354063bc7a4b
Yuvanesh-ux <68208096+Yuvanesh-ux@users.noreply.github.com>
2023-04-11 15:27:16 -0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,10 +1,8 @@
-import argparse
 import concurrent
 import concurrent.futures
 import os
 import random
 from typing import List
-import argparse
 
 import jsonlines
 from dotenv import load_dotenv
@@ -25,14 +23,13 @@ class Scraper:
         shard_size: int,
         model_settings: dict = {"max_tokens": -1},
         output_path: str = '',
-        source: str = '',
-        progress = None
+        source: str = ''
     ):
         """A method that generates responses to a list of prompts using OpenAI's GPT-3.5-turbo model and writes the output to a file.
 
         Args:
             all_prompts (List[dict]): A list of prompts as dictionary objects to generate responses to. Each dictionary object should
-            contain the 'prompt' key with a string value representing the prompt.
+            contain the 'text' key with a string value representing the prompt.
             i (int): An integer representing the starting index of the prompts to use in the all_prompts list.
             shard_size (int): An integer representing the number of prompts to generate responses for in each iteration.
             model_settings (dict, optional): A dictionary of settings to pass to the OpenAIChat model. Defaults to {"max_tokens": -1}.
@@ -43,66 +40,41 @@ class Scraper:
             Any exceptions thrown by the OpenAIChat model or jsonlines module.
 
         """
-        prompts = [d['prompt'] for d in all_prompts[i : i + shard_size]]
+        prompts = all_prompts[i : i + shard_size]
         model = OpenAIChat(
             model_name="gpt-3.5-turbo",
             openai_api_key=self.openai_api_keys[random.randint(0, len(self.openai_api_keys) - 1)],
             model_kwargs={"max_tokens": -1}, # -1 specifies we want the maximum number of tokens that can be generated
         )
-        for prompt in prompts:
+        for prompt in tqdm(prompts):
             output = model(prompt)
             with jsonlines.open(output_path, mode="a") as writer:
                 try:
-                    json_data = {"turn_0_question": prompt, "turn_0_answer": output, "model_settings": model_settings, "source": source}
+                    json_data = {"00": prompt, "01": output, "model_settings": model_settings, "source": source, "00_len": len(prompt), "01_len": len(output)}
                     writer.write(json_data)
                 except (KeyboardInterrupt, ValueError, IndexError):
                     logger.warning("Something went wrong with this prompt! Skipping to next one")
-                    with jsonlines.open(output_path + "_fails.jsonl", mode="a") as writer:
+                    with jsonlines.open(os.path.join(output_path, "fails.jsonl"), mode="a") as writer:
                         writer.write(prompt)
-            progress.update(1)
-
-    def worker(self,progress):
-        while not my_queue.empty():
-            d = my_queue.get()
-            self.get_responses(i=d[0], all_prompts=d[1], shard_size=d[2], output_path=d[3], source=d[4],progress=progress)
-            my_queue.task_done()
 
     def collector(self, 
         all_prompts: List[dict],
         num_workers: int = 10,
-        shard_size: int = 50,
+        shard_size: int = 200,
         output_path: str = '',
         source: str = ''
     ):
         logger.info("Generating Pairs")
 
-        progress = tqdm(total=len(all_prompts))
-        for i in range(0, len(all_prompts), shard_size):
-               my_queue.put([i, all_prompts, shard_size, output_path, source])
-        for i in range(threads_to_start):
-                t = threading.Thread(target=self.worker, daemon=True, args=[progress]) 
-                t.start()
-        my_queue.join()
-         
-if __name__ == "__main__":
-    # Parse command line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input_file", help="Input file name")
-    parser.add_argument("output_file", help="Output file name")
-    parser.add_argument("-k", "--openai_api_key", help="OpenAI API key")
-    args = parser.parse_args()
+        progress = tqdm(total=len(all_prompts) // shard_size)
+        with concurrent.futures.ProcessPoolExecutor(max_workers=num_workers) as executor:
+            futures = [executor.submit(self.get_responses, i=i, all_prompts=all_prompts, shard_size=shard_size, output_path=output_path, source=source) for i in range(0, len(all_prompts), shard_size)]
 
-    # Get the OpenAI API key
-    
-    if os.environ.get("OPENAI_API_KEY"):
-        openai_api_key = os.environ.get("OPENAI_API_KEY")
-    elif args.open_api_key:
-        openai_api_key = args.open_api_key
-    else:
-        print("You need an api key!")
-        exit()
-    # Create a Scraper instance
-    scraper = Scraper(openai_api_keys=[openai_api_key])
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    future.result()
+                except Exception as e:
+                    logger.exception(f"Error processing prompt: {e}")
 
                 progress.update(1)
 
