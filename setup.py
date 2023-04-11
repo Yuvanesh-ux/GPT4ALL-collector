f8acec60ca6c9995142dc38abbb67a22487e2661
Yuvanesh-ux <68208096+Yuvanesh-ux@users.noreply.github.com>
2023-04-11 18:49:49 +0000

    final

--- setup.py
+++ setup.py
@@ -4,25 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="your_project_name",
+    name="GPT4ALL-collector",
     version="0.1.0",
     author="Yuvanesh Anand",
     description="A semi scalabale system of mass scraping the chatgpt API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yuvanesh-ux/prompt-scrape",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    url="https://github.com/Yuvanesh-ux/GPT4ALL-collector",
     install_requires=[
         "python-dotenv",
         "langchain",
         "openai",
         "tqdm",
-        "jsonlines"
+        "jsonlines",
+        "loguru"
     ],
     python_requires='>=3.6',
 )
